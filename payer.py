d7ab4c221b4a8ddb72245e656d4fe423da53f255
Eleneum <140322009+Eleneum@users.noreply.github.com>
2023-09-14 05:04:55 +0200

    some changes
    txs control.
    ophan blocks control.
    block maturity = 30

--- payer.py
+++ payer.py
@@ -7,46 +7,72 @@ from eth_utils import to_checksum_address
 from ethereum.transactions import Transaction
 import requests
 from datetime import datetime
+import time
+import json
+from eth_account import Account
+
+def load_config(filename):
+    global sender_private_key
+    global sender_address
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print("Error: File " + str(filename) + " not exists")
+        exit(1)
+    except json.JSONDecodeError as e:
+        print("Error: File " + str(filename) + " is not valid")
+        exit(1)
+
+    if 'private_key' not in data:
+        print("Error: File " + str(filename) + " does not contains any private key")
+        exit(1)
+
+    try:
+        sender_private_key = data['private_key']
+        sender_account = Account.from_key(sender_private_key)
+        sender_address = sender_account.address
+    except Exception as e:
+        print("Error: File " + str(filename) + " contains an invalid private key")
+        exit(1)
+    
+    print("Eleneum server started with address: " + sender_address)
 
 w3 = Web3()
 
-sender_address = 'SENDER_ADDRESS'
-sender_private_key = 'YOUR_PRIVATE_KEY'
+sender_address = ''
+sender_private_key = ''
+
+load_config('eleneum.json')
 
 client = MongoClient('mongodb://localhost:27017')
 db = client['eleneum']
 poolpayments_collection = db['poolpayments']
-
+pooltxs_collection = db['pooltransactions']
+txs_collection = db['transactions']
 filter = {'status': -1}
-
 results = poolpayments_collection.find(filter)
 
 address_sums = {}
 for result in results:
     address = result['address']
     value = result['value']
-
     if address in address_sums:
         address_sums[address] += value
     else:
         address_sums[address] = value
-
     poolpayments_collection.update_one({'_id': result['_id']}, {'$set': {'status': 1}})
 
-
 for address, sumvalue in address_sums.items():
-    svalue = (sumvalue/1000000000000000000)
+    svalue = sumvalue/1000000000000000000
     print(f"Address: {address}, SumValue: {svalue}")
-
+#    svalue -= 0.000053
     print(f"Fee value: {svalue}")
     try:
-
-        recipient_address = to_checksum_address(address)#.lower()
+        recipient_address = to_checksum_address(address)
         value = w3.to_wei(svalue, 'ether')
-
         nonce = datetime.now().timestamp()
         nonce = int(nonce)
-
         tx = {
             'nonce': nonce,
             'to': recipient_address,
@@ -55,14 +81,20 @@ for address, sumvalue in address_sums.items():
             'gasPrice': w3.to_wei('1', 'gwei'),
             'chainId': 666999
         }
-
         signed_tx = w3.eth.account.sign_transaction(tx, sender_private_key)
         print(str(signed_tx.rawTransaction.hex()))
-
-        url = 'http://localhost:8080/addtransaction'
-        response = requests.post(url, data=str(signed_tx.rawTransaction.hex()))
-        print(response.json())
+        ts = int(time.time())
+        pooltxs_collection.insert_one({'timestamp': ts, 'value': str(value), 'rawtx': str(signed_tx.rawTransaction.hex())})
     except ValueError as e:
         print(e)
     except Exception as e:
         print(e)
+
+for r in pooltxs_collection.find():
+    z = txs_collection.find({'rawtx':r['rawtx']})
+    if 'hash' in z:
+        continue
+    url = 'http://pool.eleneum.org:9090/addtransaction'
+    response = requests.post(url, data=str(r['rawtx']))
+    data = response.json()
+    print(data)
--- unlocker.py
+++ unlocker.py
@@ -1,28 +1,63 @@
 from pymongo import MongoClient
+import time
+import json
+from eth_account import Account
 
+def load_config(filename):
+    global sender_private_key
+    global sender_address
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print("Error: File " + str(filename) + " not exists")
+        exit(1)
+    except json.JSONDecodeError as e:
+        print("Error: File " + str(filename) + " is not valid")
+        exit(1)
+
+    if 'private_key' not in data:
+        print("Error: File " + str(filename) + " does not contains any private key")
+        exit(1)
+
+    try:
+        sender_private_key = data['private_key']
+        sender_account = Account.from_key(sender_private_key)
+        sender_address = sender_account.address
+    except Exception as e:
+        print("Error: File " + str(filename) + " contains an invalid private key")
+        exit(1)
+    
+    print("Eleneum server started with address: " + sender_address)
+
+sender_address = ''
+sender_private_key = ''
+
+load_config('eleneum.json')
 
 client = MongoClient('mongodb://localhost:27017')
-db = client['eleneum']  # Nombre de tu base de datos
-poolblocks_collection = db['poolblocks']  # Nombre de tu colección "poolblocks"
-poolsoloblocks_collection = db['poolsoloblocks']  # Nombre de tu colección "poolblocks"
-info_collection = db['info']  # Nombre de tu colección "poolblocks"
-transactions_collection = db['transactions']  # Nombre de tu colección "transactions"
-pool_collection = db['pool']  # Nombre de tu colección "pool"
+db = client['eleneum']
+poolblocks_collection = db['poolblocks']
+poolsoloblocks_collection = db['poolsoloblocks']
+blocks_collection = db['blocks']
+transactions_collection = db['transactions']
+pool_collection = db['pool']
 poolpayments_collection = db['poolpayments']
-
 query = {"$or": [{"reward": {"$exists": False}}, {"reward": "0"}]}
 poolblocks = poolblocks_collection.find(query)
 
 pplns = 1
 
-num_records = transactions_collection.count_documents({})
-info_query = {"info": "totaltx"}
-info_update = {"$set": {"value": num_records}}
-info_collection.update_one(info_query, info_update, upsert=True)
-
-# Prev height
 query = {"reward": {"$ne": "0"}}
 prevhashq = poolblocks_collection.find(query).sort("ts", -1).limit(1)
+
+z = blocks_collection.find_one(sort=[("height", -1)])
+try:
+    actualheight = int(z['height'])
+except:
+    actualheight = 1
+
+print(actualheight)
 
 try:
     pblock = prevhashq.next()
@@ -34,8 +69,12 @@ except Exception as e:
 
 for block in poolblocks:
     height = block['height']
+    
+    if actualheight - 30 < int(height):
+        continue
+    
     if pplns == 1:
-        pplnsheight = height - 50
+        pplnsheight = height - 100
         if previous_height < pplnsheight:
             pplnsheight = previous_height
     else:
@@ -45,34 +84,33 @@ for block in poolblocks:
     transaction = transactions_collection.find_one(transaction_query)
     
     if transaction:
-        value = transaction['txinfo']['value']
+        if transaction['txinfo']['sender'].lower() == sender_address.lower():
+            value = transaction['txinfo']['value']
+        else:
+            value = -1
         poolblocks_collection.update_one({"_id": block["_id"]}, {"$set": {"reward": value}})
         print(f"Height: {height}, Value: {value} agregado a poolblocks")
 
-    pool_query = {"solo": 0, "block": {"$gt": int(pplnsheight), "$lte": int(height)}}
-    pool_records = pool_collection.find(pool_query)
-    
-    address_totals = {}
-    totals = 0
-
-    payable = round(int(value) * 0.98, 4)
-    for record in pool_records:
-        address = record['address']
-        target = record['target']
-        totals += target
-        if address in address_totals:
-            address_totals[address] += target
-        else:
-            address_totals[address] = target
-  
-    for address, total in address_totals.items():
-        if address not in bwallets:
+    if int(value) > 0:
+        pool_query = {"solo": 0, "block": {"$gt": int(pplnsheight), "$lte": int(height)}}
+        pool_records = pool_collection.find(pool_query)
+        address_totals = {}
+        totals = 0
+        payable = round(int(value) * 0.98, 4)
+        for record in pool_records:
+            address = record['address']
+            target = record['target']
+            totals += target
+            if address in address_totals:
+                address_totals[address] += target
+            else:
+                address_totals[address] = target
+      
+        for address, total in address_totals.items():
             btotal = total / totals
             pay = btotal * payable
             r = poolpayments_collection.insert_one( { 'address': address, 'block': height, 'value': pay, 'status': -1 } )
-
     #pool_collection.delete_many({"block": {"$lte": int(height)}})
-
     previous_height = int(height)
 
 query = {"$or": [{"reward": {"$exists": False}}, {"reward": "0"}]}
@@ -82,29 +120,29 @@ print("SOLO:")
 for block in poolsoloblocks:
     height = block['height']
     
+    if actualheight - 30 < int(height):
+        continue
+
     transaction_query = {"block": int(height), "type": 1}
     transaction = transactions_collection.find_one(transaction_query)
-    
+
     if transaction:
-        value = transaction['txinfo']['value']
+        if transaction['txinfo']['sender'].lower() == sender_address.lower():
+            value = transaction['txinfo']['value']
+        else:
+            value = -1
         poolsoloblocks_collection.update_one({"_id": block["_id"]}, {"$set": {"reward": value}})
         print(f"Height: {height}, Value: {value} agregado a poolblocks")
-
-    address_totals = {}
-    totals = 0
-
-    payable = round(int(value) * 0.98, 4)
-    fee = round(int(value) * 0.02, 4)
     
-    address_totals["FEE_ADDRESS_HERE"] = fee
-    address = block['address']
-    if address in address_totals:
-        address_totals[address] += payable
-    else:
-        address_totals[address] = payable
-  
-    for address, total in address_totals.items():
-        #print(f"Address: {address}, Total: {total}, Total pct: {btotal}, Paid: {total}")
-        r = poolpayments_collection.insert_one( { 'address': address, 'block': height, 'value': total, 'status': -1 } )
-    #r = poolpayments_collection.insert_one( { 'address': "", 'block': 100000000000+int(height), 'value': fee, 'status': -1 } )
+    if int(value) > 0:
+        address_totals = {}
+        totals = 0
+        payable = round(int(value) * 0.98, 4)
+        address = block['address']
+        if address in address_totals:
+            address_totals[address] += payable
+        else:
+            address_totals[address] = payable
+        for address, total in address_totals.items():
+            r = poolpayments_collection.insert_one( { 'address': address, 'block': height, 'value': total, 'status': -1 } )
     #pool_collection.delete_many({"block": {"$lte": int(height)}})
